e53941f10ec6e41661006bbbed6ba7592e3a434d
Dorion Mitchell <dorionmitchell@Dorions-MBP.local>
2023-10-30 21:06:16 -0400

    saved files

--- models.py
+++ models.py
@@ -1,54 +1,84 @@
+"""Models for flask-feedback."""
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
-db = SQLAlchemy()
-
 bcrypt = Bcrypt()
+db = SQLAlchemy()
 
 
 def connect_db(app):
-    """Connect to database."""
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
 
     db.app = app
     db.init_app(app)
 
+
 class User(db.Model):
     """Site user."""
 
-    __tablename__ = "user"
+    __tablename__ = "users"
 
+    username = db.Column(
+        db.String(20),
+        nullable=False,
+        unique=True,
+        primary_key=True,
+    )
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
 
-    username = db.Column(db.Text(20), 
-                         primary_key=True,
-                         nullable=False)
-    password = db.Column(db.Text, 
-                         nullable=False)
-    email = db.Column(db.Text(50), nullable=False)
-    first_name = db.Column(db.Text(30), nullable=False)
-    last_name = db.Column(db.Text(30), nullable=False)
+    feedback = db.relationship("Feedback", backref="user", cascade="all,delete")
+
+    # start of convenience class methods
 
     @classmethod
-    def register(cls, username, pwd):
-        """Register user w/hashed password & return user."""
+    def register(cls, username, password, first_name, last_name, email):
+        """Register a user, hashing their password."""
 
-        hashed = bcrypt.generate_password_hash(pwd)
-        # turn bytestring into normal (unicode utf8) string
+        hashed = bcrypt.generate_password_hash(password)
         hashed_utf8 = hashed.decode("utf8")
+        user = cls(
+            username=username,
+            password=hashed_utf8,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
 
-        # return instance of user w/username and hashed pwd
-        return cls(username=username, password=hashed_utf8)
-    
+        db.session.add(user)
+        return user
+
     @classmethod
-    def authenticate(cls, username, pwd):
+    def authenticate(cls, username, password):
         """Validate that user exists & password is correct.
 
         Return user if valid; else return False.
         """
 
-        u = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first()
 
-        if u and bcrypt.check_password_hash(u.password, pwd):
-            # return user instance
-            return u
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
         else:
             return False
+
+
+class Feedback(db.Model):
+    """Feedback."""
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(
+        db.String(20),
+        db.ForeignKey('users.username'),
+        nullable=False,
+    )
